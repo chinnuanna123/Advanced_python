4e6e4850debe8a4380c5b1d46136e34b2e40203f
kurianchinnuanna@gmail.com <kurianchinnuanna@gmail.com>
2025-01-24 17:09:10 +0530

    Adding Files

---
+++ lambda _funct.py
@@ -0,0 +1,32 @@
+#A simple lambda function to add two numbers.
+#add = lambda a,b : a+b
+#print(add(2,3))
+#Define a lambda function that multiplies two numbers.
+#multi = lambda a,b: a*b
+#print(multi(2,3))
+#max_value = lambda x,y: x if x>y else y
+#print((max_value(5,6)))
+#A lambda function that checks if a number is even or odd.
+#number = lambda x: "Even" if x % 2 ==0 else "Odd"
+#n = int(input("Enter the NUmber:"))
+#print(number(n))
+#Create a lambda function that returns "Yes" if a number is greater than 10, and "No" otherwise.
+#Number = lambda x: "Yes" if x>10 else "No"
+#n =int(input("Enter a Nummber:"))
+#print(Number(n))
+#Use Lambda and map() to Add 2 to Each Number
+#numbers =[1,2,3,4,5]
+#result = map(lambda x:2+x,numbers)
+#print(list(result))
+#Using lambda with map() to square a list of numbers
+#numbers =[1,2,3,4,5]
+#result = map(lambda x:x*x,numbers)
+#print(list(result))
+#num1 = [1, 2, 3]
+#num2 = [10, 20, 40]
+#result = map(lambda n1, n2: n1+n2, num1, num2)
+#print(list(result))
+#actions=['eat', 'sleep','read']
+# convert each string into list of individual characters 
+#result= list(map(list,actions))
+#print(result)
