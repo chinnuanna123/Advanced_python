4de2ecd72b8c387563e1b50c39f1164d7e3f598d
kurianchinnuanna@gmail.com <kurianchinnuanna@gmail.com>
2025-01-24 17:07:16 +0530

    Adding Files

---
+++ Day6/Exceptional_handling.py
@@ -0,0 +1,22 @@
+#Write code to perform division that could cause an error if the divisor is zero.
+#try:
+    #number = int(input("Enter the Number :"))
+    #print(1/number)
+#except ZeroDivisionError:
+       #print("You cannot divide with Zero")
+#except ValueError:
+      #print("Please Enter an integer value") 
+#finally:
+      #print("Execution Completes")    
+#Write code that tries to access an index of a string that may be out of range.
+#Use a finally block to print a message indicating that the operation is complete.
+#try:
+    #Name = ["Chinnu","Anna","Kurian","jis"]
+    #print(Name[4])
+#except:IndexError
+    #print("please check with the index")
+#finally:
+    #print("Execution completes")
+
+
+
