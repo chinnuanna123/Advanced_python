ad8c21c64faa185a982080d0b2a8224fd43c56c6
kurianchinnuanna@gmail.com <kurianchinnuanna@gmail.com>
2025-01-27 16:20:09 +0530

    Adding Files

---
+++ Day3/module.py
@@ -0,0 +1,4 @@
+import my_module
+#output=my_module.greet()
+#print(my_module.square(4))
+print(my_module.area(3))
---
+++ Day3/my_module.py
@@ -0,0 +1,48 @@
+#def greet():
+#   print("Hello,Good Morning!!")
+#examples
+#import math#
+#import math as m
+#from math import pi
+#pi = 3.14
+#def square(x):
+    #return x**2
+#def cube(x):
+#    return x**3
+#def circumference(r):
+    #return 2*pi*r
+#def area(r):
+    #return pi*r**2
+#Use pandas to read a CSV file and display the first few rows.
+#import pandas as p 
+#df = p.read_csv(r"D:\details.csv")
+#print(df.to_string())
+#Output
+#     Name  Rank        Date
+#0  Chinnu     3  12-01-2020
+#1    Anju     2  12-01-2020
+#2   Tobin     1          11
+#3  Akhila     4  21-21-2021
+#print(df.head(2))
+#output
+#     Name  Rank        Date
+#0  Chinnu     3  12-01-2020
+#1    Anju     2  12-01-2020
+#Using the pandas module to create a simple DataFrame from a dictionary.
+#my_data = {
+#    "Name": ["Alice","Bob","Alan"],
+#    "Age":[34,12,6]
+#          }
+#data = p.DataFrame(my_data)
+#print(data)
+#Import a module with a different name using as
+#import math as m
+#print(m.e)
+#output
+#2.718281828459045
+#Import the datetime module as dt and get the current date.
+#import datetime as dt
+#print(dt.date.today())
+#print(dt.datetime.now())
+
+
