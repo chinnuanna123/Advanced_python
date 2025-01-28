b90e2e358864c184c788e688a4247c44fad95dfd
kurianchinnuanna@gmail.com <kurianchinnuanna@gmail.com>
2025-01-28 17:03:38 +0530

    Adding Files

---
+++ Day4/Class.py
@@ -0,0 +1,79 @@
+#Define a Simple Student Class with variables & Methods
+#class Student:
+   # name ="Chinnu"
+    #rollno = 13  
+    #def display(self,rank):
+        #print(f"The rank of the student {Student.name} is {rank}")
+
+#Student1 = Student()
+#print(Student1.name)
+#print(Student1.rollno)
+#Student1.display(3)
+
+
+# Output
+#Chinnu
+#13
+#The rank of the student Chinnu is 3
+
+
+#Create a class called Person with the following attribute name
+#class Person:
+ #   name = "Alan"
+#    Age = 12
+#person1 = Person()  
+#print(person1.name)
+#print(person1.Age)  
+
+
+#Output
+#Alan
+#12
+
+#Define a Car Class with Methods and create a Object
+#An instance of a class with specific attributes & Methods 
+
+
+#class Car:
+#    def __init__(self,model,year,color):
+#        self.model = model
+#        self.year = year
+#        self.color = color
+#    def display(self,version):
+#        print(f"My new model {self.model} car in {self.color} color released in the year {self.year} of version {version}")
+
+#my_car = Car("Exter",2024,"White") #Instantiate the class to create an object and Call Methods 
+#print(my_car.model)
+#print(my_car.year)
+#print(my_car.color)
+#my_car.display("sx")
+
+
+#output
+#Exter
+#2024
+#White
+#My new model Exter car in white color released in the year 2024 of version sx
+
+
+
+#class Employee:
+    #def __init__(self,name,position,salary):
+        #self.name = name
+        #self.position = position
+        #self.salary = salary
+
+    #def display(self):
+        #return f"Name: {self.name}; Position: {self.position}; Salary: {self.salary}"
+    #def increment(self,amount):
+        #self.salary +=amount
+        #return f"{self.name} got salary increment of rs {amount};New salary is {self.salary}"
+#employee1 = Employee("Aditya","DevOps Engineeer",50000)
+#details = employee1.display()    
+#print(details)
+#rise = employee1.increment(1000)
+#print(rise)
+
+# OUTPUT
+#Name: Aditya; Position: DevOps Engineeer; Salary: 50000
+#Aditya got salary increment of rs 1000;New salary is 51000
